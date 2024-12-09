ddc5882be6adee3ec00eb7f222c7f33e2b3af173
naveedahmed02 <60284159+naveedahmed02@users.noreply.github.com>
2024-12-09 12:01:36 +0500

    Update On-Demand-elk-Deployment.sh

--- On-Demand-elk-Deployment.sh
+++ On-Demand-elk-Deployment.sh
@@ -1,6 +1,6 @@
-DEPLOYMENT_NAME = "elk-deployment"
-DOCKER_COMPOSE_FILE_NAME = "docker-compose.yml"
-ENV_FILE = "env"
+DEPLOYMENT_NAME="elk-deployment"
+DOCKER_COMPOSE_FILE_NAME="docker-compose.yml"
+ENV_FILE="env"
 
 echo "The service list is:" 
 list=`echo $Services | sed -r "s/,+/ /g" `
